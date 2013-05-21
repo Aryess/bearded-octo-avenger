fed915c320ba0aaa6b5279eb7f08d92fd8d71eae
Aryess <yoann.celton@gmail.com>
2013-05-21 11:12:13 -0400

    Rails set up, ready to go

---
+++ bearded-squid/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeardedSquid::Application.config.secret_token = '87157e49b1e5e588e964d4d9b5f987dfffdb7fec5da2d71ac04ae2fb225ef4d9e55b47609083c92260891e4a216438540e3509c7c4ceb45e549381d74965ce90'
