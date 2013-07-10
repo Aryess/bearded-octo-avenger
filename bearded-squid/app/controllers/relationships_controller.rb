d0a9b0f69d717acd8429eeb6a842756e77e65fbd
Aryess <yoann.celton@gmail.com>
2013-07-10 04:35:10 -0400

    Finishing feature

--- bearded-squid/app/controllers/relationships_controller.rb
+++ bearded-squid/app/controllers/relationships_controller.rb
@@ -2,18 +2,27 @@ class RelationshipsController < ApplicationController
   before_filter :signed_in_user, only: [:create, :destroy]
   def create
     @user = User.find_by_id(params[:relationship][:followed_id])
-    if @user != current_user
-      current_user.follow!(@user)
-      flash[:success] = "Following user"
+    current_user.follow!(@user)
+    respond_to do |format|
+      format.html do 
+        flash[:success] = "Following user"
+        redirect_to @user
+      end
+      format.js
     end
-    redirect_to @user
+    
   end
   
   def destroy
     @user = Relationship.find_by_id(params[:id]).followed
     current_user.unfollow!(@user)
-    flash[:success] = "User unfollowed"
-    redirect_to @user
+    respond_to do |format|
+      format.html do 
+        flash[:success] = "User unfollowed"
+        redirect_to @user
+      end
+      format.js
+    end
   end
   
 end
--- bearded-squid/app/models/user.rb
+++ bearded-squid/app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
   has_many :followers, through: :reverse_relationships
   
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
   
   def following?(other_user)
